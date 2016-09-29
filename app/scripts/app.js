cc8404db5a8e3f4aaf3998ffe3f1e7a8cb8b878d
Fournier <efournier@pch.com>
2016-09-29 15:38:42 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name stockDashboardApp
+ * @description
+ * # stockDashboardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('stockDashboardApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stockDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stockDashboardApp
+ */
+angular.module('stockDashboardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
